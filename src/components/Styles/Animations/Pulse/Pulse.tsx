d8102a6b99c2f44ef6081a25c6571a89e35bf475
22sook00 <22sook00@gmail.com>
2023-03-01 00:33:46 +0900

    Add new skeleton style as shimmer

--- src/components/Styles/Animations/Pulse/Pulse.tsx
+++ src/components/Styles/Animations/Pulse/Pulse.tsx
@@ -1,19 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Pulse.scss";
+
 const Pulse = () => {
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		setIsLoading(true);
+		setTimeout(() => {
+			setIsLoading(false);
+		}, 1000);
+	}, []);
 	return (
-		<section className="pulse-container">
-			<div className="pulse-animation-wrapper">
-				<div className="pulse-circle-item"></div>
-				<div className="pulse-space-wrapper">
-					<div className="pulse-each-items"></div>
+		<>
+			<h1>shimmer</h1>
+			<div>
+				{!isLoading ? (
+					<li className="skeleton-item">
+						<div>
+							<div className="skeleton-img" />
+						</div>
+						<div className="skeleton-info">
+							{new Array(4).fill(1).map((_, i) => {
+								return <div key={i}>내용{i}</div>;
+							})}
+						</div>
+					</li>
+				) : (
+					<li className="skeleton-item">
+						<div>
+							<div className="skeleton-img" />
+						</div>
+						<div className="skeleton-info">
+							<p className="skeleton-name" />
+							<p className="skeleton-email" />
+							<p className="skeleton-email" />
+							<p className="skeleton-desc" />
+						</div>
+					</li>
+				)}
+			</div>
+			<h1>pulse</h1>
+			<section className="pulse-container">
+				<div className="pulse-animation-wrapper">
+					<div className="pulse-circle-item"></div>
 					<div className="pulse-space-wrapper">
-						<div className="pulse-each-items" />
-						<div className="pulse-each-items" />
+						<div className="pulse-each-items"></div>
+						<div className="pulse-space-wrapper">
+							<div className="pulse-each-items" />
+							<div className="pulse-each-items" />
+						</div>
 					</div>
 				</div>
-			</div>
-		</section>
+			</section>
+		</>
 	);
 };
 
